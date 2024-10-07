6aff01437bb734f14188c8e5a50f2648593d44dc
jwoo9 <jwyutong@gmail.com>
2024-10-07 14:18:30 +0000

    ready for grading - 10/7

--- WordSorter.java
+++ WordSorter.java
@@ -37,7 +37,6 @@ public class WordSorter
 			word = word.replace("?", "");
 			word = word.replace("'", "");
 			word=word.toLowerCase();
-			System.out.println(word);
 			char letter = word.charAt(0);
 			if (checkDupe(word,letter-97,wordList)==false)
 				wordList[letter-97].add(word);	
@@ -54,6 +53,7 @@ public class WordSorter
 			System.out.println("6. Exit");
 			int choice = input.nextInt();
 			input.nextLine();
+
 			while (choice <1 || choice >6){
 				System.out.println("Please enter a valid input:");
 				choice = input.nextInt();
@@ -72,8 +72,41 @@ public class WordSorter
 					System.out.println("No words start with this letter");
 				else
 					printLetter(wordList, letter-97);
-			} else if (choice ==2)
-				printAll(wordList);
+
+
+			} else if (choice == 2){
+				if (getSize(wordList)==0)
+					System.out.println("Empty List");
+				else
+					printAll(wordList);
+
+
+			} else if (choice == 3){
+				int unique = getSize(wordList);
+				if (unique == 0)
+					System.out.println("Empty List");
+				else
+					System.out.println("There are " + unique + " unique words in the list");
+
+
+			} else if (choice == 4){
+				System.out.println("what word would you like to find?");
+				String findWord = input.nextLine();
+				search(wordList,findWord,1);
+
+
+			} else if (choice == 5){
+				System.out.println("What word would you like to remove?");
+				String findWord = input.nextLine();
+				int index = search(wordList,findWord,0);
+				int letter = findWord.charAt(0)-97;
+				if (index!=-1){
+					wordList[letter].remove(index);
+					System.out.println("Word successfully removed from list");
+				} //end if
+			} else if (choice == 6)
+				runAgain=false;
+			
 		}//end runAgain while
 
 	}//end main
@@ -88,19 +121,39 @@ public class WordSorter
 	}//end method notDupe
 
 	public static void printLetter(ArrayList<String>[] arr, int index){
-		for (int i = 0; i<arr[index].size(); i++)
+		for (int i = 0; i<arr[index].size(); i++){
 			System.out.println(arr[index].get(i));
+		}//end for
 	}//end method printLetter
 
-	public static int printAll(ArrayList<String>[] arr){
+	public static void printAll(ArrayList<String>[] arr){
 		char x = 97;
 		int numWords =0;
-		for (int i = 0; i<26; i++){
+		for (int i = 0; i<arr.length; i++){
 			System.out.println(x + ": ");
 			printLetter(arr,i);
 			x++;
 		}
-		return numWords;
 	} //end method printAll
+
+	public static int getSize(ArrayList<String>[]arr){
+		int size = 0;
+		for (int i =0; i<arr.length; i++)
+			size+=arr[i].size();
+		return size;
+	}//end method getSize 
+
+	public static int search(ArrayList<String>[]arr, String word, int type){
+		int index = word.charAt(0) -97;
+		for (int i = 0; i<arr[index].size();i++){
+			if (arr[index].get(i).equals(word)){
+				if (type == 1)
+					System.out.println("Word found in article");
+				return i;	
+			}//end if
+		}//end for
+		System.out.println("Word NOT found in article");
+		return -1;
+	}//end method search
 
 }//end class
